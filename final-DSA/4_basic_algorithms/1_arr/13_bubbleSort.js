d601c6b52b29d012e594583ee161073d68e262ae
satyampal <em.satyam@gmail.com>
2025-12-09 12:21:52 +0400

    minor changes

--- final-DSA/4_basic_algorithms/1_arr/13_bubbleSort.js
+++ final-DSA/4_basic_algorithms/1_arr/13_bubbleSort.js
@@ -1,14 +1,13 @@
 // bubble Sort
-const arr = [ 7,12,9,11,3 ]
+const arr = [ 7,14,11,8,9]
 
-for(let i = 0; i<arr.length; i++){
-    for(let j = i; j<arr.length; j++){
-      if(arr[i] > arr[j]){
+for(let i = 0; i<arr.length-1; i++){
+    for(let j = 0; j<arr.length-1-i; j++){
+      if(arr[i] > arr[j+1]){
          let initial = arr[i];
-         arr[i] = arr[j];
-         arr[j] = initial;
+         arr[i] = arr[j+1];
+         arr[j+1] = initial;
       }
     }
   }
-
 console.log(arr);
